f22b22d3f40d8dfab9efe34cb0f509e785c9fb4e
Manouchehr Abdi <mabdi0258@gmail.com>
2025-03-11 23:25:29 +0330

    create route vue and tailwind V4

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-2xl font-bold">This is an about page</h1>
+    <p class="text-xl text-red-500">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, officia
+      nesciunt nostrum exercitationem accusantium, commodi ullam dolorem
+      expedita nisi recusandae dolorum placeat! Ullam perspiciatis commodi
+      repudiandae sapiente nisi, saepe maxime accusantium optio molestias, natus
+      magni voluptates culpa possimus fugit earum harum dolorem similique quae
+      nam quibusdam beatae maiores! Molestiae, repudiandae?
+    </p>
   </div>
 </template>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="Jobs">
+    <h1 class="text-6xl font-bold text-blue-400">Hello Manouchehr</h1>
+    <img class="ml-70 mt-10" alt="Vue logo" src="../assets/logo.png" />
+
+  </div>
+</template>
