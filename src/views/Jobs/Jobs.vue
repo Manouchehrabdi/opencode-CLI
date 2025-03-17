caf41d5f16773a4e50ffc91218db2e1e99380da5
Manouchehr Abdi <mabdi0258@gmail.com>
2025-03-17 10:47:53 -0800

    Edit color navbar Menu Jobs

--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -2,7 +2,7 @@
   <h1>Jobs</h1>
   <div v-for="job in jobs" :key="job.id">
     <h2
-      class="font-mono font-bold bg-[#f4f4f4] p-3 rounded-xl m-3  ju cursor-pointer hover:bg-sky-300"
+      class="font-mono font-bold bg-[#e5e0e0] p-3 rounded-xl m-3  ju cursor-pointer hover:bg-sky-300"
     >
       {{ job.title }}
     </h2>
